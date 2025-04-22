9ed2b6c050a0fb5facf8222f6e9bb57e6c22dfbe
TanHao <justinxie2406@gmail.com>
2025-04-22 11:27:47 +0700

    018

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  const n = "Tấn Hào ";
+  return (
+    <>
+      <div className="App" style={{ display: "flow-root" }}>
+        <h1 style={{ color: "#5eae91" }}>{n}Hello word</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
