65c90095da8c6168a59eb93a386d26b42249b30c
TanHao <justinxie2406@gmail.com>
2025-04-22 10:38:32 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  const n = "Tấn Hào ";
+  return (
+    <>
+      <div className="App">
+        <h1>{n}Hello word</h1>
+      </div>
+      <div className="App">
+        <h1>{n}Hello word</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
