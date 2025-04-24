16c17cdd81f3be8cf5cecd1ff61e6bfdcbd57587
TanHao <justinxie2406@gmail.com>
2025-04-24 22:16:27 +0700

    024 4 phút

--- src/App.js
+++ src/App.js
@@ -1,56 +1,15 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import TodoItem from "./TodoItem";
 
 function App() {
-  const [todos, setTodos] = useState([
-    { name: "Đi học", id: "111" },
-    { name: "Đi ngủ", id: "112" },
-  ]);
-  const [inputState, setInputState] = useState("");
-  const [isEdit, setIsEdit] = useState(false);
-
-  const handleAddTodo = () => {
-    setTodos((prev) => [...prev, { name: inputState, id: Math.random() }]);
-  };
-
-  const handleDeleteTodo = (idDelete) => {
-    const filtered = todos.filter((item) => item.id !== idDelete);
-    setTodos(filtered);
-  };
-
-  const handleUpdateTodo = (id, newName) => {
-    const updatedTodos = todos.map((todo) =>
-      todo.id === id ? { ...todo, name: newName } : todo
-    );
-    setTodos(updatedTodos);
-  };
-
+  const [count, setCount] = useState(0);
+  useEffect(() => {});
   return (
-    <>
-      <div className="App">
-        <div>
-          <input
-            value={inputState}
-            onChange={(e) => {
-              setInputState(e.target.value);
-            }}
-          />
-          <input onClick={handleAddTodo} type="button" value="Add" />
-        </div>
-
-        {todos.map((todo, index) => {
-          return (
-            <TodoItem
-              key={todo.id}
-              todo={todo}
-              handleDeleteTodo={handleDeleteTodo}
-              handleUpdateTodo={handleUpdateTodo}
-            />
-          );
-        })}
-      </div>
-    </>
+    <div className="App">
+      <h2>Helo</h2>
+      <div>{count}</div>
+      <button onClick={() => setCount(count + 1)}>Add</button>
+    </div>
   );
 }
 
