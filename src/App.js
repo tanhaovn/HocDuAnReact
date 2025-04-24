69f60921fbb6b56778f6929381d9212614a03b37
TanHao <justinxie2406@gmail.com>
2025-04-24 15:37:26 +0700

    video 22 2 phut

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React, { useState } from "react";
-// import Card from "./Card";
 import "./App.css";
 
 function App() {
-  // const [count, setCount] = useState(1);
-  // const handleCount = () => {
-  //   setCount(count + 1);
-  //   console.log(count);
-  // };
-  const [todos, setdTodos] = useState([{ name: "Đi học" }, { name: "Đi ngủ" }]);
+  const [todos, setTodos] = useState([{ name: "Đi học" }, { name: "Đi ngủ" }]);
   const [inputState, setInputState] = useState("");
+  const [isEdit, setIsEdit] = useState(false);
+
+  const handleAddTodo = () => {
+    setTodos((prev) => [...prev, { name: inputState }]);
+  };
+
+  const handleDeleteTodo = (nameDelete) => {
+    const filtered = todos.filter((item) => item.name !== nameDelete);
+    setTodos(filtered);
+  };
+
   return (
     <>
-      {/* <div className="App">
-        <div>Count: {count}</div>
-        <button onClick={handleCount}>More</button>
-      </div> */}
       <div className="App">
         <div>
           <input
@@ -24,9 +25,22 @@ function App() {
               setInputState(e.target.value);
             }}
           />
+          <input onClick={handleAddTodo} type="button" value="Add" />
         </div>
+
         {todos.map((todo, index) => {
-          return <div key={index}>{todo.name}</div>;
+          return (
+            <div key={index} className="todo-item">
+              {isEdit ? (
+                <input value={todo.name} />
+              ) : (
+                <>
+                  <h3 onClick={() => setIsEdit(!isEdit)}>{todo.name}</h3>
+                  <span onClick={() => handleDeleteTodo(todo.name)}>X</span>
+                </>
+              )}
+            </div>
+          );
         })}
       </div>
     </>
